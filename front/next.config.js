f78ce49929deb00f941044987ce1c717cfef4968
bgdtc <theo.bogdan@hotmail.com>
2022-10-17 16:44:02 +0200

    socket-io init

--- front/next.config.js
+++ front/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  env: {
+    API_GET_ALL_USERS: "https://192.168.1.39:3000/users",
+    API_ENDPOINT: "https://192.168.1.39:3000/"
+  }
 }
 
 module.exports = nextConfig
