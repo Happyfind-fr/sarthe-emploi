90c148a9367bbb2b7ecc9c06fe1586da5c655729
bgdtc <theo.bogdan@hotmail.com>
2022-10-17 11:39:15 +0200

    oop

--- back/src/database/models/users_model.ts
+++ back/src/database/models/users_model.ts
@@ -8,30 +8,34 @@ export class User {
         this!.id = user.id || 0; this!.lastName = user.lastName || '';
         this!.firstName = user.firstName || ''; this!.avatar = user.avatar || '';
         this!.email = user.email || ''; this!.password = user.password || '';
-    }
+    };
 
     async getAllUsers(query?: string) {
-        const data = await getAll("users", query)
-        return data;
-    }
-    async getUserById(query?: string) {
-        const data = await getOne("users", this.id, query)
+        const data = await getAll("users", query);
         return data.rows;
-    }
+    };
+
+    async getUserById(query?: string) {
+        const data = await getOne("users", this.id, query);
+        return data.rows;
+    };
+
     async createUser() {
         const data = await create("users", "firstname,lastname,email,password",
             `'${this.lastName}', '${this.firstName}', '${this.email}', '${this.password}'`);
-        return data;
-    }
+        return data.rows;
+    };
+
     async updateUser() {
         const ar: any = [];
-        Object.entries(this).forEach(kv => kv[1] === '' || kv[0] === 'id' ? '' : ar.push(` ${kv[0]} = '${kv[1]}'`))
-        const data = await update("users", `${ar.toString()}`, this.id)
-        return data;
-    }
+        Object.entries(this).forEach(kv => kv[1] === '' || kv[0] === 'id' ? '' : ar.push(` ${kv[0]} = '${kv[1]}'`));
+        const data = await update("users", `${ar.toString()}`, this.id);
+        return data.rows;
+    };
+
     async deleteUser() {
-        const data = await remove("users", this.id)
-        return data
-    }
+        const data = await remove("users", this.id);
+        return data.rows;
+    };
 
 }
