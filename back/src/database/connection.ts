0eccde5fc7e3f1cd7f0c80b31601e42993a15bee
bgdtc <theo.bogdan@hotmail.com>
2022-10-17 11:41:19 +0200

    fix-client

--- back/src/database/connection.ts
+++ back/src/database/connection.ts
@@ -1,10 +1,4 @@
-import { Pool, Client } from 'pg';
+import { Client } from 'pg';
 require('dotenv').config();
 
-const pool = new Pool({
-    max: 2000,
-    connectionString: process.env.POSTGRES_CONNECTION,
-    idleTimeoutMillis: 30000
-});
 export const client = new Client(`${process.env.POSTGRES_CONNECTION}`)
-export default pool
