c9bff111ed9bdeb448887f38065e4a814ea5d953
bgdtc <theo.bogdan@hotmail.com>
2022-10-18 15:21:56 +0200

    front-arch

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store } from "./store";
+import HomePage from "./pages/HomePage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </HashRouter>
+    </Provider>
+
   );
 }
-
-export default App;
